33c13baa940913749ca145bf1d3805bf9bfdb247
674230026 <674230026@webmail.npru.ac.th>
2026-03-29 01:52:35 +0700

    Add files via upload

--- miniproject.js
+++ miniproject.js
@@ -20,7 +20,7 @@ listmenu.forEach(item => {
     // ลบ active ทุกเมนู
     listmenu.forEach(i => i.classList.remove('active'));
     this.classList.add('active');
-    
+
     // หา target
     let target = this.querySelector('a').getAttribute('href');
 
@@ -40,31 +40,6 @@ listmenu.forEach(item => {
   });
 });
 
-// ====== เปิด/ปิด card edit ======
-var cards = document.querySelectorAll(".row-profile-ctl");
-var item1 = document.querySelector(".card-edit-ctl");
-var item2 = document.querySelector(".row-card-catalog");
-var cancel = document.querySelector(".btn-cancle");
-
-cards.forEach(function(card) {
-  card.addEventListener("click", function (e) {
-    e.stopPropagation();
-    card.classList.add("active");
-    item1.classList.add("active");
-    item2.classList.add("active");
-
-  });
-});
-
-if (cancel) {
-  cancel.addEventListener("click", function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    item1.classList.remove("active");
-    item2.classList.remove("active");
-
-  });
-}
 
 //เปิดหน้าแก้ไข้สินค้า
 var ctgpd = document.getElementById("catalogproduct");
@@ -73,31 +48,136 @@ var editctg = document.getElementById("editcatalog");
 
 btnctg.addEventListener("click", function () {
   ctgpd.classList.toggle("active");
-  editctg.classList.toggle("active"); 
+  editctg.classList.toggle("active");
 });
 
-//เปิดหน้าแก้ไข้สินค้า
-var btnellipsis = document.q("btnellipsis");
-var btneditctl = document.querySelector(".group-btn-edit");
-var btnangle= document.querySelector(".btn-angle-left");
 
-btnellipsis.addEventListener("click", function () {
-  btnellipsis.classList.add("active");
-  btneditctl.classList.add("active"); 
+document.querySelectorAll('#editcatalog').forEach(box => {
+
+  // หา element ปุ่ม 3 จุด ภายใน box นั้น
+  const card = box.querySelector('.row-profile-ctl');
+  const item1 = box.querySelector('.card-edit-ctl');
+  const item2 = box.querySelector('.row-card-catalog');
+  const cancleBtn = box.querySelector('.btn-cancle');
+  const menuBtn = box.querySelector('.btn-menu-ctl');
+
+  // ===== เมื่อกดปุ่ม card =====
+  card.addEventListener('click', (e) => {
+    e.preventDefault();                 // ป้องกันการทำงาน default (เช่น <a> เด้งหน้า)
+    box.classList.add('active');
+    item1.classList.add("active");
+    item2.classList.add("active");       
+    menuBtn.classList.add("close");       
+  });
+
+  // ===== เมื่อกดปุ่มกลับ (x) =====
+  cancleBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    cancleBtn.classList.remove('active');
+    item1.classList.remove("active");
+    item2.classList.remove("active");
+    menuBtn.classList.remove("close");    
+  });
+
 });
 
-btnangle.addEventListener("click", function () {
-  btnellipsis.classList.remove("active");
-  btneditctl.classList.remove("active"); 
+
+// เลือก element ทุกตัวที่มี class .btn-menu-ctl
+document.querySelectorAll('.btn-menu-ctl').forEach(box => {
+
+  // หา element ปุ่ม 3 จุด ภายใน box นั้น
+  const toggleBtn = box.querySelector('.btn-ellipsis');
+  const backBtn = box.querySelector('.btn-angle-left');
+
+  // ===== เมื่อกดปุ่ม 3 จุด =====
+  toggleBtn.addEventListener('click', (e) => {
+    e.preventDefault();                 // ป้องกันการทำงาน default (เช่น <a> เด้งหน้า)
+    box.classList.add('active');
+    toggleBtn.classList.add("active");        
+  });
+
+  // ===== เมื่อกดปุ่มกลับ (⟵) =====
+  backBtn.addEventListener('click', () => {
+    box.classList.remove('active');
+    toggleBtn.classList.remove("active");     
+  });
+
 });
 
-// var containers = document.querySelectorAll(".container");
 
-var light = document.querySelector(".light");
-var dark = document.querySelector(".dark");
-var body = document.querySelector("body");
 
-light.addEventListener("click", function () {
-  light.classList.toggle("active");
-  body.classList.toggle("active");
+// เปิดปิดหน้า crate,admin-edit 
+var adminedit = document.getElementById("admin-edit");
+var createedit = document.getElementById("create-edit");
+var member = document.getElementById("member");
+
+// เปิดหน้า admin edit
+document.querySelectorAll(".edit").forEach(function (btn) {
+  btn.addEventListener("click", function () {
+
+    //  ปิดทุก container ก่อน
+    sections.forEach(function (sec) {
+      sec.classList.remove("active");
+    });
+
+    //  แล้วค่อยเปิด admin-edit
+    adminedit.classList.add("active");
+
+  });
+
+});
+
+// เปิดหนเ้า create edit
+document.querySelectorAll(".btn-add-member").forEach(function (btnadd) {
+  btnadd.addEventListener("click", function () {
+
+    //  ปิดทุก container ก่อน
+    sections.forEach(function (sec) {
+      sec.classList.remove("active");
+    });
+
+    //  แล้วค่อยเปิด admin-edit
+    createedit.classList.add("active");
+
+  });
+});
+// ปิดหน้า crate,admin-edit 
+document.querySelectorAll(".btn-cancle-member").forEach(function (btncancle) {
+  btncancle.addEventListener("click", function () {
+
+    // แล้วค่อยเปิด admin-edit
+    member.classList.add("active");
+    createedit.classList.remove("active");
+    adminedit.classList.remove("active");
+
+  });
+});
+
+// profile page 
+var profile = document.getElementById("profile");
+var listsales = document.getElementById("listsales");
+
+document.querySelectorAll("#btn-profile").forEach(function (btnprofile) {
+  btnprofile.addEventListener("click", function () {
+
+    //  ปิดทุก container ก่อน
+    sections.forEach(function (sec) {
+      sec.classList.remove("active");
+    });
+
+    // แล้วค่อยเปิด admin-edit
+    profile.classList.add("active");
+
+  });
+});
+
+//ปิดหน้า crate,admin-edit 
+document.querySelectorAll(".btn-cancle-profile").forEach(function (btncancleprofile) {
+  btncancleprofile.addEventListener("click", function () {
+
+    // แล้วค่อยเปิด admin-edit
+    profile.classList.remove("active");
+    listsales.classList.add("active");
+
+  });
 });
